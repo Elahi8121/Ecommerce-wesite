efbbd0d6164efbd3c0de97f672c0a1253c2c7bc9
elahi8121 <ferozilahi1@gmail.com>
2019-09-15 23:25:35 +0530

    first commit

---
+++ JS.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#corausal").slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay:true,
+      autoplaySpeed:1100
+      
+  });
+});
